7c47af6c0b300fc722f4905e7b099db6c9437713
Bryan Li <bryanli0@icloud.com>
2020-10-02 17:56:04 -0400

    Started working with Firebase.

--- Focus-Tracker/AppDelegate.swift
+++ Focus-Tracker/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        Firebase.configure()
         AppSettings.registerDefaults()
         return true
     }
---
+++ Pods/Target Support Files/Pods-Focus-Tracker/Pods-Focus-Tracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Focus_Tracker : NSObject
+@end
+@implementation PodsDummy_Pods_Focus_Tracker
+@end
