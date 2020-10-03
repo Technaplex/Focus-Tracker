b613115dc96f69a3d515d8e99a564635424329ff
Nonameentered <matthewshu88@gmail.com>
2020-10-02 20:12:22 -0400

    remove unnecessary focus segmentedcontrol

--- Focus-Tracker/CreateTimerViewController.swift
+++ Focus-Tracker/CreateTimerViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class CreateTimerViewController: UIViewController {
     @IBOutlet weak var type: UISegmentedControl!
-    @IBOutlet weak var focus: UISegmentedControl!
     @IBOutlet weak var activity: UITextField!
     @IBOutlet weak var goal: UITextField!
     
     @IBAction func start(_ sender: Any) {
         let presenter = presentingViewController as! UITabBarController
         
+        // Definitely feels like there's a better way to do this
         let timer = (presenter.viewControllers![0] as! UINavigationController).topViewController as! CurrentTimerViewController
         
-        timer.typeLabel.text = "Text: \(focus.titleForSegment(at: focus.selectedSegmentIndex)!) \(type.titleForSegment(at: type.selectedSegmentIndex)!)"
+        timer.typeLabel.text = "Text: Mindful \(type.titleForSegment(at: type.selectedSegmentIndex)!)"
         
         timer.activity = activity.text!
         timer.running = true
