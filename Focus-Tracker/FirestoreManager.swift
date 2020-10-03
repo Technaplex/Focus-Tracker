d8de28d28d51a3ba472e0ad5f4e2c03e007d2a3a
Bryan Li <bryanli0@icloud.com>
2020-10-03 15:29:19 -0400

    Finished initial Firestore manager.

--- Focus-Tracker/FirestoreManager.swift
+++ Focus-Tracker/FirestoreManager.swift
@@ -18,6 +18,55 @@ class FirestoreManager {
     }
     
     func getSessions(){
-        return userRef.collection("sessions").return
+        return userRef.collection("sessions").getDocuments(){
+            (querySnapshot, err) in
+            if let err = err{
+                print("Error during Firestore read: \(err)")
+            } else {
+                return (querySnapshot!.documents).map({$0.data()})
+            }
+        }
+    }
+    
+    func getActivities(_ sessId: String){
+        return userRef.collection("sessions").document(sessId).collection("activities").getDocuments(){
+            (querySnapshot, err) in
+            if let err = err{
+                print("Error during Firestore read: \(err)")
+            } else {
+                return (querySnapshot!.documents).map({$0.data()})
+            }
+        }
+    }
+    
+    func getActivity(_ actId: String, sessId: String){
+        let actRef = return userRef.collection("sessions").document(sessId).collection("activities").document(actId)
+        actRef.getDocument{ (document, error) in
+            if let document = document, document.exists {
+                return document.data()
+            } else {
+                print("Error during Firestore read: \(err)")
+            }
+        }
+    }
+    
+//    func getSession(_ id: String){
+//        getSessions()
+//    }
+    
+    func addSession(_ sessId: String, data: [String: Any]){
+        userRef.collection("sessions").document(sessId).setData(data) {err in
+            if let err = err{
+                print("Error on write: \(err)")
+            }
+        }
+    }
+    
+    func addActivity(_ actId: String, sessId: String, data: [String: Any]){
+        userRef.collection("sessions").document(sessId).collection("activities").document(actId).setData(data) { err in
+            if let err = err {
+                print("Error on write: \(err)")
+            }
+        }
     }
 }
